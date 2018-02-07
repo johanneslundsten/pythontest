a2e5ea6c16d344fee9f165fcddf160c5e9ad47d3
Johannes Lundsten <johannes.lundsten@gmail.com>
2018-02-07 19:17:18 +0100

    implementation of the game

---
+++ game_with_classes.py
@@ -0,0 +1,48 @@
+import random
+
+values = ['rock', 'paper', 'scissors']
+counter = 1
+
+
+class Hand:
+
+    def __init__(self, value):
+        self.value = value
+
+    def challenge(self, hand):
+        if hand.value == self.value:
+            return "TIE"
+        elif self.value == 'rock' and hand.value == 'paper':
+            return "LOSS"
+        elif self.value == 'scissors' and hand.value == 'rock':
+            return "LOSS"
+        elif self.value == 'paper' and hand.value == 'scissors':
+            return "LOSS"
+        else:
+            return "WIN"
+
+
+def get_computer_choice():
+    return Hand(random.choice(values))
+
+
+def get_user_choice():
+    user_input = input("\n#" + str(counter) + " Enter your choice: rock, paper or scissors. (Exit to leave)\n").lower()
+    while user_input not in values:
+        if user_input == 'exit':
+            print("Bye")
+            exit()
+        user_input = input("\n Invalid input\nEnter your choice: rock, paper och scissors \n")
+
+    return Hand(user_input)
+
+
+while True:
+
+    computer_choice = get_computer_choice()
+    player_choice = get_user_choice()
+
+    result = player_choice.challenge(computer_choice)
+
+    print(result + "! Computer chose " + computer_choice.value)
+    counter = counter + 1
